53f7fe4f8f5a70718a5ff19d3ecd22acfda37a2a
Aditya Bhandari <aditya.bhandari@mailchimp.com>
2019-05-28 11:31:51 -0400

    Initial Commit

---
+++ src/lib/react/hello/hello.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class Hello extends Component {
+    state = {};
+    render() {
+        return (
+            <div className="hello">
+                The "Hello Component Library" welcomes Joanna and Aylin!
+            </div>
+        );
+    }
+}
